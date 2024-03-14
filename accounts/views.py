ef067351dbcac20f21dd75b33a68f2586921b81e
PKPY <p01001011p01011001@gmail.com>
2024-03-14 21:24:21 +0330

    [006]: used human readable ready status codes from drf

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,24 @@
+'''
+\_______________________________[STATUS-CODE]__________________________________/
+
+Status BaseCode      Means               Helper Functions
+===============      ==============      ===================
+1xx                  Informational       is_informational()
+2xx                  Successful          is_success()
+3xx                  Redirection         is_redirect()
+4xx                  Client Error        is_client_error()
+5xx                  Server Error        is_server_error()
+________________________________________________________________________________/
+'''
+# https://www.django-rest-framework.org/api-guide/status-codes/
+#_______________________________________________________________________________/
 from django.shortcuts import render
 
 from django.contrib.auth.models import User
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import UserRegisterSerializer, UserRegisterModelSerializer
 
@@ -16,6 +31,6 @@ class UserRegisterView(APIView): # create user (C in CRUD)
             data: dict = serialized_data.validated_data
             serialized_data.create(data) # Custome create method
 
-            return Response(serialized_data.data)
-        return Response(serialized_data.errors) # return serializer auto errors
+            return Response(serialized_data.data, status=status.HTTP_201_CREATED) # status used for more human readable
+        return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST) # .errors return serializer auto errors
 
